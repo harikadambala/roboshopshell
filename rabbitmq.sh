9c1bb0a98ea80946f65b1da557fc257efc21ab10
harikadambala <harikadambala131@gmail.com>
2024-03-14 02:53:41 +0530

    shell

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -49,7 +49,7 @@ systemctl start rabbitmq-server  &>> $LOGFILE
 
 VALIDATE $? "Starting rabbitmq server"
 
-rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE     
+#rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE     
 rabbitmqctl set_user roboshop roboshop123 &>> $LOGFILE 
 
 VALIDATE $? "creating user"
