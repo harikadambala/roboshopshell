728ccdbc44b2ad37c8779066b22b28585484e827
harikadambala <harikadambala131@gmail.com>
2024-03-24 17:12:06 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 AMI=ami-0f3c7d07486cad139 #this keeps on changing
-SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
+SG_ID=sg-0d7e9a4297607132a #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
 DOMAIN_NAME="daws76s.online"
