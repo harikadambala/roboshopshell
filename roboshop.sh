7b0a4d98ae0fc6d97bb5f96197329461f7f454a0
harikadambala <harikadambala131@gmail.com>
2024-03-24 17:09:35 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,10 +1,11 @@
+
 #!/bin/bash
 
-AMI=ami-0f3c7d07486cad139
-SG_ID=sg-0d7e9a4297607132a # replace with your SG ID
+AMI=ami-0f3c7d07486cad139 #this keeps on changing
+SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID= Z018402424UTMGZJ08WQL # replace your zone ID  Z10212311RJKRWFBNT8EY
-DOMAIN_NAME="daws76study.online"
+ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
+DOMAIN_NAME="daws76s.online"
 
 for i in "${INSTANCES[@]}"
 do
@@ -15,7 +16,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-0d7e9a4297607132a --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
     #create R53 record, make sure you delete existing record
@@ -37,5 +38,4 @@ do
         }]
     }
         '
-         
- done
+done
